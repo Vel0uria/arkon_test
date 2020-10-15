cceb54cef49e3cf2d0ef7302804a6cba00dfdcfb
VelOuria <sofleos@gmail.com>
2020-10-14 23:27:42 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Super Turbo Productivity App</h1>
+      <Button>Nueva tarea</Button>
+    </div>
+  );
+}
+
+export default Home;
