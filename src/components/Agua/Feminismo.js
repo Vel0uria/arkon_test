24cd39e59b7f1bfc9376a6d3e8e597f564931ee4
VelOuria <sofleos@gmail.com>
2020-10-27 22:20:53 -0600

    links feminismo

--- src/components/Agua/Feminismo.js
+++ src/components/Agua/Feminismo.js
@@ -15,45 +15,192 @@ function Feminismo() {
       />
 
       <List>
-        <ListItem />
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJA56kYly7w3I1-ow?e=7AbYcG"}
+          >
+            (Varias Autoras) Africana. Aportaciones para la descolonización del
+            feminismo. Entrevistas.
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJwsXlP_-3Z3qQjAw?e=5F3W6k"}
+          >
+            (Varias Autoras) Diálogo Y Diferencia - Los Feminismos Desafían A
+            La Globalización
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJX9Ps-e0j0b6pHog?e=08D7U8"}
+          >
+            (Varias Autoras) Feminismos En La Antropología - Nuevas Propuestas
+            Críticas
+          </a>
+        </ListItem>
         <ListItem>
           <a
-            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJQr5qdnJ5VEaTf5A?e=Jpv7tQ"}
+          >
+            (Varias autoras) Feminismos Negros. Una antologia
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJmN9OmfZUhUbFDeg?e=F8eokn"}
+          >
+            (Varias Autoras) Las Fisuras Del Patriarcado, Reflexiones sobre
+            feminismo y derecho
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJORkeYdawXUpW2Dg?e=2Sc5DD"}
+          >
+            (Varios) Cuadernos De Lectura Sobre Feminismos Trans
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJ0jZYSqXhC2HfQ2Q?e=dXw3m2"}
+          >
+            (Varios) El género en el derecho. Ensayos críticos
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJhsudNZ8rtb4xk3g?e=3umb2n"}
+          >
+            (Varios) Estudios sobre la Violencia Masculina
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJZ-GlMLzLlAyhJ2A?e=ffS6PV"}
+          >
+            (Varios) Etica y Feminismo
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaI9ayLiGRIB3Y_nuQ?e=IP4i1i"}
+          >
+            (Varios) Femicidio. Más allá de la violación del derecho a la
+            vida
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJHGL7nMKIWUJ5p3A?e=lANTMD"}
+          >
+            (Varios) Feminismo Y Cambio Social En America Latina Y El Caribe
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaInirrXj8RLc4a3lA?e=eOEfdu"}
+          >
+            (Varios) Feminismo Y Teoría Del Discurso
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaIwEuipnUZIJAI1rQ?e=AkfETN"}
+          >
+            (Varios) Feminismos Debates Teóricos Contemporáneos
+          </a>
+        </ListItem>
+
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJD_ifQc2-6_2K5lA?e=330X2y"}
+          >
+            (Varios) Géneros Prófugos. Feminismo y educación
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJF4p2opgZ84akCew?e=BZhrHa"}
+          >
+            (Varios) Historia de Las Mujeres en América Latina
+          </a>
+        </ListItem>
+
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJR0wPDn9MnaNRUMQ?e=AEuc4D"}
+          >
+            (Varios) Igualdad De Oportunidades Y Feminismo
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJ1ykOanxE0GazhVA?e=zk6PTp"}
+          >
+            (Varios) La mujer y el comunismo. Antología de Textos
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaIqhgat0wPWEzdyiA?e=iAZaPc"}
+          >
+            (Varios) Masculinidad-es, poder y crisis
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJSIGkJw02ocAq_Rg?e=nXH9FZ"}
+          >
+            (Varios) Medio Ambiente y desarrollo. Miradas Feministas desde ambos
+            hemisferios
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaMOqpJ-RbN9XQNXEA?e=d3bQie"}
+          >
+            (Varios) Mujer, Mujeres y Bioética
+          </a>
+        </ListItem>
+        <ListItem button>
+          <a
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJPbX1BcRqadcJLQg?e=zX7toH"}
           >
             (Varios) Por un Feminismo sin Mujeres
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
-            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJiEjqlWarDrbkEmg?e=SJn9Bt"}
           >
             (Varios) TransFeminismos
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
-            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaIYsgS-v4C07z6cMg?e=vfpmkF"}
           >
             (Varios) Transformaciones del trabajo desde una perspectiva
             feminista
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
-            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJsUp9CVOPXmEbDBw?e=NyXjph"}
           >
             Almonacid Galvis, María Alejandra - Diálogos Entre Arte Y
             Feminismo
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
-            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
+            href={"https://1drv.ms/b/s!AqIbM7aE-jvIgaJ74NZzGzLhoLwy3A?e=sVl23d"}
           >
             Amoros, Celia - Feminismo y filosofía
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
             href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
           >
@@ -61,7 +208,7 @@ function Feminismo() {
             Fronteras
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
             href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
           >
@@ -69,7 +216,7 @@ function Feminismo() {
             Feminismo y Marxismo
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
             href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
           >
@@ -77,14 +224,14 @@ function Feminismo() {
             maternidad, siglo XII al XX
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
             href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
           >
             Belfort Bax, Ernest - El Fraude Del Feminismo
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
             href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
           >
@@ -92,7 +239,7 @@ function Feminismo() {
             devenir
           </a>
         </ListItem>
-        <ListItem>
+        <ListItem button>
           <a
             href={"https://1drv.ms/b/s!AqIbM7aE-jvIgP9NxwD0dBKWS3gTYQ?e=jacw7q"}
           >
